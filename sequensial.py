0f61d65ae1ddaf3a55b3ae3be043e0a55308c2b0
Klaze21 <kkaze8221@gmail.com>
2021-11-02 15:21:51 +0700

    contoh

--- sequensial.py
+++ sequensial.py
@@ -1,3 +1,9 @@
 """
-
+sequensial
 """
+
+print('ibu berkata,"pergi ke toko"')
+print('budi menjawab"baik bu, apa yang harus saya beli?"')
+print('ibu menjawab,"beli 1 botol susu dan jika ada telur beli 6."')
+print("maka budi pergi ke toko")
+print("budi mulai berbelanja")
