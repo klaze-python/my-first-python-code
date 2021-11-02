64ea4157cb80901e309dd46840ac7616ad956d30
Klaze21 <kkaze8221@gmail.com>
2021-11-02 15:28:51 +0700

    contoh

---
+++ Percabangan.py
@@ -0,0 +1,14 @@
+"""
+percabangan
+"""
+
+jumlah_botol_susu = 9
+
+if jumlah_botol_susu > 10:
+    print("budi mengecek harganya dan cukup")
+    print("budi membeli 1 botol susu")
+else:
+    print("budi tidak jadi membeli 1 botol susu")
+
+print("budi pulang ke rumah")
+print("menyerahkan hasilnya kepada ibu")
